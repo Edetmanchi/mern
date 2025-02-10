2dae321e8b8f6860822823c2215d7674cfd2a34f
Edet Effiong <greatstory247@gmail.com>
2025-02-10 23:57:05 +0100

    all backend files added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+// @desc Auth user/set token
+// route POST/ api/user/auth
+// access public
+
+
+const authUser = (req, res)=>{
+    res.status(200).json({message: 'set user'})
+
+}
+
+export default {authUser}
