f167523da91d7ec91bd173fcb6fdf308ffaeff5c
Edet Effiong <greatstory247@gmail.com>
2025-02-13 23:56:00 +0100

    codebase updated

--- backend/server.js
+++ backend/server.js
@@ -288,6 +288,1086 @@ app.listen(port, ()=> {
 });
 
 
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+import express from "express"
+// import {useRoutes} from "./routes/userRoutes.js"
+
+// to use tokens from the env file
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express();
+// app.use('/api/user', useRoutes)
+
+
+app.get('/', (res, req)=> res.send("server is ready"));
+
+app.listen(port, ()=> {
+    console.log(`my server started at port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
