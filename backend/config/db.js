20718712840db869cf17cd1bfff22b0e741033f9
Edet Effiong <greatstory247@gmail.com>
2025-02-16 22:55:59 +0100

    database connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`mongoDB connected ${conn.connection.host}`)
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`mongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1)
@@ -13,4 +13,4 @@ const connectDB = async () => {
 
 }
 
-export default {connectDB}
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express"
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandlier } from "./middleware/errorMiddleware.js"
+import connectDB from './config/db.js'
 
 // to use tokens from the env file
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config()
 const port = process.env.PORT || 5000
+
+// connect mongo db
+connectDB()
 
 const app = express();
 // anything connected to api users will be coming from the userRoutes()
